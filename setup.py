9fa62c7a8db39d57fd5e9942a9bd9a8ad346a407
Julien Seiler <seilerj@igbmc.fr>
2019-09-09 21:37:55 +0200

    passage à la version 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='absents',
-    version='0.4',
+    version='0.6',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
